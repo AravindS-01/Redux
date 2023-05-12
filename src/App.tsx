b32434e662c6676d5a0936fb9ba5e8b54a50874b
AravindS-01 <aravindad13@gmail.com>
2023-05-12 16:01:09 +0530

    Initial UI commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/cardComponent/Cards';
+import BurgerCombo from './assest/Burger.png'
+import Burger from './assest/burgerImg.png'
+import riceBowl from './assest/riceBowlImage.png'
 
+
+const dishes = [
+  {
+    id: 201,
+    dishName: 'Burger Combo',
+    description: 'A tasty burger with French fries with ketchup',
+    price: 220,
+    image:BurgerCombo
+  },
+  {
+    id: 202,
+    dishName: 'Burger',
+    description: 'A tasty and yummy burger with margarto cheese',
+    price: 100,
+    image:Burger
+  },
+  {
+    id: 203,
+    dishName: 'Rice Bowl',
+    description: 'The Rice bowl contains the yummy chicken curry with flavour ghee and rice',
+    price: 250,
+    image:riceBowl
+  },
+  {
+    id: 204,
+    dishName: 'Pizza',
+    description: 'The Rice bowl contains the yummy chicken curry with flavour ghee and rice',
+    price: 200,
+    image:riceBowl
+  },
+]
 function App() {
+
+  const onAddDishes =() =>{}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className='card' style={{backgroundImage:`url(${Burger})`,backgroundSize: ' 100% 100%',backgroundRepeat: 'no-repeat',}}>
+        <ul>
+          {dishes.map(meal => {
+            return (
+              <>
+                <li className='meal_add_button'>
+                  <div>
+                    <h3>{meal.dishName}</h3>
+                    <div  style={{fontStyle:'italic'}}>{meal.description}</div>
+                    <div style={{fontWeight:'bold'}}>₹{meal.price}</div>
+                  </div>
+                  <div className='input_button_label'>
+                    <div className='input_button'>
+                    <label className='labelText'>{'Amount'}</label>
+                    <input type='number' className='input_size' min='1' defaultValue ='1'></input>
+                    </div>
+                    <button className='buttonProp' onClick={onAddDishes}>Add +</button>
+                  </div>
+                </li>
+                <hr/>
+              </>
+            )
+          })}
+        </ul>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/redux/reducer.ts
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+const cartSlicer =createSlice({
+    name:'cart',
+    initialState:'',
+    reducers:{}
+});
+
+
+
+export default cartSlicer.reducer;
