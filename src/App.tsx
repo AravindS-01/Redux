7d413e8b610d16c41477f6f5233573bdc68a8d96
AravindS-01 <aravindad13@gmail.com>
2023-05-16 22:26:31 +0530

    Implementation of empty cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,48 @@
-import React, { Fragment, useCallback, useEffect, useState } from 'react';
-import './App.css';
-import BurgerCombo from './assest/Burger.png'
-import Burger from './assest/burgerImg.png'
-import riceBowl from './assest/riceBowlImage.png'
-import MealListItem from './MealListItem';
-import Header from './header/Header';
-import { useAppDispatch, useAppSelector } from './redux/hook';
-import Dish from './redux/types';
-import { removeMeal } from './redux/reducer';
-
+import React, { Fragment, useCallback, useEffect, useState } from "react";
+import "./App.css";
+import BurgerCombo from "./assest/Burger.png";
+import Burger from "./assest/burgerImg.png";
+import riceBowl from "./assest/riceBowlImage.png";
+import MealListItem from "./MealListItem";
+import Header from "./header/Header";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+import Dish from "./redux/types";
+import { emptyCart, removeMeal } from "./redux/reducer";
 
 const dishes = [
   {
     id: 201,
-    dishName: 'Burger Combo',
-    description: 'A tasty burger with French fries with ketchup',
+    dishName: "Burger Combo",
+    description: "A tasty burger with French fries with ketchup",
     price: 220,
-    image: BurgerCombo
+    image: BurgerCombo,
   },
   {
     id: 202,
-    dishName: 'Burger',
-    description: 'A tasty and yummy burger with margarto cheese',
+    dishName: "Burger",
+    description: "A tasty and yummy burger with margarto cheese",
     price: 100,
-    image: Burger
+    image: Burger,
   },
   {
     id: 203,
-    dishName: 'Rice Bowl',
-    description: 'The Rice bowl contains the yummy chicken curry with flavour ghee and rice',
+    dishName: "Rice Bowl",
+    description:
+      "The Rice bowl contains the yummy chicken curry with flavour ghee and rice",
     price: 250,
-    image: riceBowl
+    image: riceBowl,
   },
   {
     id: 204,
-    dishName: 'Pizza',
-    description: 'The Rice bowl contains the yummy chicken curry with flavour ghee and rice',
+    dishName: "Pizza",
+    description:
+      "The Rice bowl contains the yummy chicken curry with flavour ghee and rice",
     price: 200,
-    image: riceBowl
+    image: riceBowl,
   },
-]
+];
 function App() {
-  const selector = useAppSelector(state => state.cart)
+  const selector = useAppSelector((state) => state.cart);
   const [cartCount, setCartCount] = useState(0);
   const dispatch = useAppDispatch();
 
@@ -49,67 +50,106 @@ function App() {
     const data = selector.dishes.reduce((previousValue, currentValue) => {
       return Number(previousValue) + Number(currentValue.numberOfItem);
     }, 0);
-    setCartCount(data)
+    setCartCount(data);
   }, [selector.dishes]);
 
-  const removeItem = useCallback((dish:Dish)=>{
-       const reduceItem = {
+  const removeItem = useCallback(
+    (dish: Dish) => {
+      const reduceItem = {
         ...dish,
-        numberOfItem: Number(dish.numberOfItem) - 1
-       };
- dispatch(removeMeal(reduceItem))
-  },[dispatch])
+        numberOfItem: Number(dish.numberOfItem),
+      };
+      dispatch(removeMeal(reduceItem));
+    },
+    [dispatch]
+  );
+ const emptycart = useCallback(() =>{
+  dispatch(emptyCart());
+ },[dispatch])
+  console.log("selector", selector);
   return (
     <>
       <Fragment>
         <Header></Header>
-        <div className='card' style={{ backgroundImage: `url(${Burger})`, backgroundSize: ' 100% 100%', backgroundRepeat: 'no-repeat', }}>
+        <div
+          className="card"
+          style={{
+            backgroundImage: `url(${Burger})`,
+            backgroundSize: " 100% 100%",
+            backgroundRepeat: "no-repeat",
+          }}
+        >
           <ul>
-            {dishes.map(meal => {
+            {dishes.map((meal) => {
               return (
                 <>
                   <MealListItem meal={meal}></MealListItem>
                 </>
-              )
+              );
             })}
           </ul>
         </div>
       </Fragment>
-      <div className='card'>
-        <span className='cartDetail'>Cart Details</span>
-        {selector && !!selector.dishes.length && selector.dishes.map(item => {
-          return (
-            <>
-              <li style={{ display: 'flex',justifyContent:'space-between' ,alignItems:'center'}}>
-                <div className='dispaly'>
-                  <h3 style={{ margin: '8px' }}>{item.dishName}</h3>
-                  <div style={{ fontWeight: "bold" }}>
-                    <span>Food Price: </span>
-                    <span>₹{item.price}</span></div>
-                  <div style={{ display: 'flex' }}>
-                    <span>{'Number of Items Ordered: '}</span>
-                    <span style={{ fontWeight: "bold" }}>{item.numberOfItem}</span></div>
-                </div>
-                <button className='reduceButton' onClick={() =>{removeItem(item)}}>Reduce Item in Cart</button>
-              </li>
-              <hr />
-            </>
-          )
-        })
-        }
-
-        <p style={{ fontSize: '24px', fontWeight: 700, display: 'flex', flexDirection: 'column' }}>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <span>{'No.Of.Items Ordered'}</span>
+      <div className="card">
+        <span className="cartDetail">Cart Details</span>
+        {selector &&
+          !!selector.dishes.length &&
+          selector.dishes.map((item) => {
+            return (
+              <>
+                <li
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}
+                >
+                  <div className="dispaly">
+                    <h3 style={{ margin: "8px" }}>{item.dishName}</h3>
+                    <div style={{ fontWeight: "bold" }}>
+                      <span>Food Price: </span>
+                      <span>₹{item.price}</span>
+                    </div>
+                    <div style={{ display: "flex" }}>
+                      <span>{"Number of Items Ordered: "}</span>
+                      <span style={{ fontWeight: "bold" }}>
+                        {item.numberOfItem}
+                      </span>
+                    </div>
+                  </div>
+                  <button
+                    className="reduceButton"
+                    onClick={() => {
+                      removeItem(item);
+                    }}
+                  >
+                    Reduce Item in Cart
+                  </button>
+                </li>
+                <hr />
+              </>
+            );
+          })}
+        <p
+          style={{
+            fontSize: "24px",
+            fontWeight: 700,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <span>{"No.Of.Items Ordered"}</span>
             <span>{`${cartCount}`}</span>
           </div>
-          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-            <span>{'TotalAmount'}</span>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <span>{"TotalAmount"}</span>
             <span>₹{`${selector.totalAmount}`}</span>
           </div>
         </p>
-
+        {!!selector.dishes.length && (<button className ='emptyCart'onClick={emptycart}> Empty cart</button>) }
       </div>
+      
     </>
   );
 }
--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -43,10 +43,14 @@ const cartSlicer = createSlice({
         state.totalAmount -= action.payload.price;
         let alreadyExistedItemIndex = state.dishes.findIndex(item => item.id === action.payload.id);
             let existedItem = state.dishes[alreadyExistedItemIndex];
-            if(existedItem){
+            if(Number(existedItem.numberOfItem) === 1){
+                state.dishes = state.dishes.filter((item) => {  return item.id !== action.payload.id})
+
+            }
+            else{
                 const reduceItem = {
                     ...existedItem,
-                    numberOfItem: Number(existedItem.numberOfItem)
+                    numberOfItem: Number(existedItem.numberOfItem) - 1,
                 };
                 state.dishes = state.dishes.map((dish) => {
                     if (dish.id === action.payload.id) {
@@ -57,16 +61,14 @@ const cartSlicer = createSlice({
                     }
                 });
             }
-            else {
-                state.dishes.push(action.payload);
-                // state = {...state, dishes:[...state.dishes,action.payload]} // both are same
-
-            }
-
-    }
+    },
+    emptyCart:(state) =>  {
+          state.dishes=[];
+          state.totalAmount = 0;
+    },
     }
 });
 
 
-export const { addMeal , removeMeal} = cartSlicer.actions;
+export const { addMeal , removeMeal, emptyCart} = cartSlicer.actions;
 export default cartSlicer.reducer;
